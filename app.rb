fb2de368d2542b3783134cca426d0cd1f056dcd4
wbosw <146214919+wbosw@users.noreply.github.com>
2023-10-27 23:03:21 +0000

    initial calc temp

--- app.rb
+++ app.rb
@@ -4,6 +4,25 @@ require "sinatra/reloader"
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
+    <form>
+      <button>
+      Calcualate Square
+      </button>
+    </form>
+
   "
+end
+
+get("/square/new") do
+  erb(:square_new)
+end
+
+get("/square/results") do
+  pp @user_number = params.fetch("square")
+
+  pp @squared_user_number = params.fetch("square").to_f ** 2
+  erb(:square_results)
+end
+
+get("/4444") do
 end
